77bdaf0f811f2f9d4976d75bfd69ff0cef5f89e3
Alaan07 <rakeshofficial007@gmail.com>
2024-04-05 22:43:27 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ function App() {
   return (
     <div>
     <h1 className="bg-purple-500"> Hello world</h1>
-      
+      <h1 className="bg-orange-500">Testing 123</h1>
+      <h1 className="bg-yellow-500">Testing 123</h1>
     </div>
   )
 }
