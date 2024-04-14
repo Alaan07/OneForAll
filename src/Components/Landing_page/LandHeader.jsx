02a1c72a01ff71ddcf3e6b4d82f39ab7fcba55cc
Alaan07 <rakeshofficial007@gmail.com>
2024-04-14 19:37:07 +0530

    improved navbar and header and resposive of lading page

--- src/Components/Landing_page/LandHeader.jsx
+++ src/Components/Landing_page/LandHeader.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import LandNavbar from './LandNavbar'
+import React from "react";
+import { Link } from "react-router-dom";
+import LandNavbar from "./LandNavbar";
 
 function LandHeader() {
   return (
-    <div>
-    <div className=" bg-gray-900 min-h-[600px]">
+    <div className="min-h-screen bg-gray-900">
     <LandNavbar />
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col items-center max-w-4xl w-full px-6">
+        <h1 className="text-white font-bold text-5xl md:text-6xl lg:text-7xl text-center p-10">
+          Design & High Quality
+        </h1>
+        <p className="text-gray-500 text-lg md:text-xl lg:text-2xl text-center px-10">
+          "Unbeatable deals on everything you need! Shop now and save big on
+          our entire collection. Don't miss out!"
+        </p>
+        <button className="border m-5 border-gray-500 bg-transparent text-gray-500 rounded-3xl py-2 px-6 hover:bg-green-300 hover:text-black hover:font-bold" ><Link to="/store">Shop now</Link></button>
+      </div>
     </div>
     </div>
-  )
+  );
 }
 
-export default LandHeader
+export default LandHeader;
--- src/Components/Landing_page/LandNavbar.jsx
+++ src/Components/Landing_page/LandNavbar.jsx
@@ -1,28 +1,24 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import MenuIcon from '@mui/icons-material/Menu';
-import ClearIcon from '@mui/icons-material/Clear';
+import MenuIcon from "@mui/icons-material/Menu";
+import ClearIcon from "@mui/icons-material/Clear";
 
 function LandNavbar() {
   const [menuToggle, setMenuToggle] = useState(false);
 
   function toggleMenu() {
-    setMenuToggle(prevMenuToggle => !prevMenuToggle);
+    setMenuToggle((prevMenuToggle) => !prevMenuToggle);
   }
 
-  function menuX(){
-    setMenuToggle(false)
+  function menuX() {
+    setMenuToggle(false);
   }
-
-
-
-
   return (
     <div>
-      <nav className="flex justify-between py-4 px-4 text-lg text-white">
+      <nav className="flex justify-between py-4 px-4 text-lg text-white bg-transparent">
         <div className="text-gray-500 hide">
           <ul className="flex landnav">
-            <li> 
+            <li>
               <Link to="/">Home</Link>
             </li>
             <li>
@@ -50,38 +46,44 @@ function LandNavbar() {
               <Link to="/login">Log In</Link>
             </li>
             <li className="px-4 SadwitchMenu">
-              <button onClick={toggleMenu}><MenuIcon/></button>
+              <button onClick={toggleMenu}>
+                <MenuIcon />
+              </button>
             </li>
           </ul>
         </div>
       </nav>
 
-        <div className="text-gray-500  p-4">
-        
-          <ul style={menuToggle ? {display : "flex"} : { display : "none"}} className="sidebar landnav flex-col absolute  right-0 top-0 min-h-full bg-slate-300 w-[400px] ">
-            <li onClick={menuX} className="MenuX">
-             <button ><ClearIcon/></button>
-            </li>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/store">Store</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-            <li className="px-4">
-              <Link to="/signin">Sign In</Link>
-            </li>
-            <li className="px-4">
-              <Link to="/login">Log In</Link>
-            </li>
-          </ul>
-        </div>
+      <div className="text-gray-500">
+        <ul
+          style={menuToggle ? { display: "flex" } : { display: "none" }}
+          className="sidebar landnav flex-col absolute  right-0 top-0 bg-slate-300 w-[400px] h-screen"
+        >
+          <li onClick={menuX} className="MenuX">
+            <button className="absolute right-8">
+              <ClearIcon />
+            </button>
+          </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/store">Store</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li className="px-4">
+            <Link to="/signin">Sign In</Link>
+          </li>
+          <li className="px-4">
+            <Link to="/login">Log In</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
